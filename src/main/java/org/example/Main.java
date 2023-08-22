0b60acf8beff2048a016d49798905e49cca67efa
Shohel <Shohel@cel.local>
2023-08-22 11:14:17 +0600

    single comment and multi line comment  commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+//        This is my new learing project
+
+        /*
+        now i learn multiple line comment
+                in java project
+           */
+
         System.out.println("Hello world!");
     }
 }
